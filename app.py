1c521a04f6052f70fd61e77bfc5ad12555ced6cb
Lin Lin Hlaing <linlinhlaing@Lins-Air.lan>
2024-09-18 15:33:47 -0500

    added application file

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
+data = {
+    1: {"name": "Lin Lin Hlaing", "age": 26},
+    2: {"name": "Hnin Thant Yati", "age": 12}
+}
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
 
-@app.route('/predict')
+# just demo function for future predition model
+@app.route('/predict', methods=['GET','POST'])
 def predict():
-    return "predict"
+    return jsonify(data)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001) 
+    # app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, port=5000) 
+
+
